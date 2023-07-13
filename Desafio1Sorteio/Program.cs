dbbfc327f117d64d4ed729af89b15e341114ecb6
Anderson <86ander@gmail.com>
2023-07-13 14:11:56 -0300

    jogo do sorteio

---
+++ Desafio1Sorteio/Program.cs
@@ -0,0 +1,22 @@
+﻿int numeroInformado;
+Random sorteia = new Random();
+int numeroSorteado = sorteia.Next(1, 101);
+for (int i=1; i<=10; i++)
+{
+    Console.WriteLine("Digite um número");
+    numeroInformado = Convert.ToInt32(Console.ReadLine());
+    if (numeroInformado == numeroSorteado)
+    {
+        Console.WriteLine("Párabens, você acertou o número");
+        i = 10;
+    } else if (numeroInformado > numeroSorteado)
+        Console.WriteLine("Digite um número menor");
+    else
+        Console.WriteLine("Digite um numero maior");
+    if (i == 10 && numeroInformado != numeroSorteado)
+    {
+        Console.WriteLine("Suas tentativas acabaram, infelizmente você não acertou o número");
+        Console.WriteLine("O número sorteado era " + numeroSorteado);
+    }
+}
+
